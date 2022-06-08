6f6a36545fb82b627254812135824e7f3a7d0ec7
Medeiros <hsymetry@gmail.com>
2022-06-08 16:43:47 -0300

    Home widget -  Dropdown button

---
+++ lib/app/modules/home/widgets/header_project_menu.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:job_timer/app/entities/projetct_status.dart';
+
+class HeaderProjectMenu extends SliverPersistentHeaderDelegate {
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return LayoutBuilder(
+      builder: ((context, constraints) {
+        return Container(
+          height: constraints.maxHeight,
+          color: Colors.white,
+          padding: const EdgeInsets.all(10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              SizedBox(
+                width: constraints.maxWidth * .5,
+                child: DropdownButtonFormField<ProjectStatus>(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    contentPadding: EdgeInsets.all(10),
+                    isCollapsed: true,
+                  ),
+                  items: ProjectStatus.values
+                      .map(
+                        (e) => DropdownMenuItem(
+                          value: e,
+                          child: Text(e.label),
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (value) {},
+                ),
+              ),
+              SizedBox(
+                width: constraints.maxWidth * .4,
+                child: ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.add),
+                  label: const Text('Novo projeto'),
+                ),
+              ),
+            ],
+          ),
+        );
+      }),
+    );
+  }
+
+  @override
+  double get maxExtent => 80.0;
+
+  @override
+  double get minExtent => 80.0;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
+  }
+}
