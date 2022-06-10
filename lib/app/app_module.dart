012bd62c91662859d43226dc58fe9c7bafaa7392
Medeiros <hsymetry@gmail.com>
2022-06-10 00:10:02 -0300

    Add Bind project repository and project Service

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,11 +1,16 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:job_timer/app/core/ui/database/database.dart';
-import 'package:job_timer/app/core/ui/database/database_impl.dart';
+import 'package:job_timer/app/core/database/database.dart';
+import 'package:job_timer/app/core/database/database_impl.dart';
 import 'package:job_timer/app/modules/home/home_module.dart';
 import 'package:job_timer/app/modules/login/login_module.dart';
+import 'package:job_timer/app/modules/project/project_module.dart';
 import 'package:job_timer/app/modules/splash/splash_page.dart';
+import 'package:job_timer/app/repositories/projects/project_repository.dart';
+import 'package:job_timer/app/repositories/projects/project_repository_impl.dart';
 import 'package:job_timer/app/services/auth/auth_services.dart';
 import 'package:job_timer/app/services/auth/auth_services_impl.dart';
+import 'package:job_timer/app/services/auth/projects/project_service.dart';
+import 'package:job_timer/app/services/auth/projects/project_service_impl.dart';
 
 class AppModule extends Module {
   @override
@@ -13,6 +18,10 @@ class AppModule extends Module {
         //Binding para ser utilizado em qualquer parte da aplicação
         Bind.lazySingleton<AuthServices>((i) => AuthServicesImpl()),
         Bind.lazySingleton<Database>((i) => DatabaseImpl()),
+        Bind.lazySingleton<ProjectRepository>(
+            (i) => ProjectRepositoryImpl(database: i())),
+        Bind.lazySingleton<ProjectService>(
+            (i) => ProjectServiceImpl(projectRepository: i()))
       ];
   @override
   List<ModularRoute> get routes => [
@@ -27,6 +36,10 @@ class AppModule extends Module {
         ModuleRoute(
           '/home/',
           module: HomeModule(),
+        ),
+        ModuleRoute(
+          '/project/',
+          module: ProjectModule(),
         )
       ];
 }
