1d736c433fd5f1d466339993144a4b2b400c6675
Medeiros <hsymetry@gmail.com>
2022-06-13 12:22:07 -0300

    Add new entities for project task implementation

--- lib/app/repositories/projects/project_repository.dart
+++ lib/app/repositories/projects/project_repository.dart
@@ -1,7 +1,13 @@
 import 'package:job_timer/app/entities/project.dart';
+import 'package:job_timer/app/entities/project_task.dart';
 import 'package:job_timer/app/entities/projetct_status.dart';
 
 abstract class ProjectRepository {
   Future<void> register(Project project);
   Future<List<Project>> findMyStatus(ProjectStatus status);
+
+  //Botão para salvar uma nova task
+  Future<Project> findById(int projectId);
+  Future<Project> addTask(int projectId, ProjectTask task);
+
 }
