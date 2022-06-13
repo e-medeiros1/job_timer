00c77a3649a782d6bfa5ff695e0f50414ad04128
Medeiros <hsymetry@gmail.com>
2022-06-12 23:58:29 -0300

    Add filter per Id and addTask

--- lib/app/services/auth/projects/project_service.dart
+++ lib/app/services/auth/projects/project_service.dart
@@ -1,7 +1,12 @@
+import 'package:job_timer/app/entities/project_task.dart';
 import 'package:job_timer/app/entities/projetct_status.dart';
 import 'package:job_timer/app/view_models/project_model.dart';
+import 'package:job_timer/app/view_models/project_task_model.dart';
 
 abstract class ProjectService {
   Future<void> register(ProjectModel projectModel);
   Future<List<ProjectModel>> findMyStatus(ProjectStatus status);
+  Future<ProjectModel> findById(int projectId);
+  Future<ProjectModel> addTask(int projectId, ProjectTaskModel task);
+
 }
