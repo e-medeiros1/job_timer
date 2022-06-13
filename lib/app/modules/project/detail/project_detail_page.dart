85580692e3074de64ea7ad3de5e8373d470486bb
Medeiros <hsymetry@gmail.com>
2022-06-13 12:28:20 -0300

    Add BlocConsumer to the detail page and extract components for better code read

--- lib/app/modules/project/detail/project_detail_page.dart
+++ lib/app/modules/project/detail/project_detail_page.dart
@@ -1,46 +1,104 @@
+import 'package:asuka/asuka.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:job_timer/app/core/ui/job_timer_icons.dart';
+import 'package:job_timer/app/modules/project/detail/widget/controller/project_detail_controller.dart';
 import 'package:job_timer/app/modules/project/detail/widget/project_detail_appbar.dart';
 import 'package:job_timer/app/modules/project/detail/widget/project_pie_chart.dart';
 import 'package:job_timer/app/modules/project/detail/widget/project_task_tile.dart';
+import 'package:job_timer/app/view_models/project_model.dart';
 
 class ProjectDetailPage extends StatelessWidget {
-  // ProjectDetailController controller;
+  final ProjectDetailController controller;
 
-  const ProjectDetailPage({Key? key}) : super(key: key);
+  const ProjectDetailPage({Key? key, required this.controller})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          ProjectDetailAppbar(),
-          SliverList(
-            delegate: SliverChildListDelegate(
-              [
-                const Padding(
-                  padding: EdgeInsets.only(top: 50, bottom: 50),
-                  child: ProjectPieChart(),
+      body: BlocConsumer<ProjectDetailController, ProjectDetailState>(
+          bloc: controller,
+          listener: (context, state) {
+            if (state.status == ProjectDetailStatus.failure) {
+              AsukaSnackbar.alert('Erro interno');
+            }
+          },
+          builder: (context, state) {
+            final projectModel = state.projectModel;
+
+            switch (state.status) {
+              case ProjectDetailStatus.initial:
+                return Column(
+                  children: const [
+                    Center(
+                      child: Text('Carregando projeto...'),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    CircularProgressIndicator.adaptive(),
+                  ],
+                );
+
+              case ProjectDetailStatus.loading:
+                return const Center(
+                  child: CircularProgressIndicator.adaptive(),
+                );
+              case ProjectDetailStatus.complete:
+                return _buildProjectDetail(context, projectModel!);
+
+              case ProjectDetailStatus.failure:
+                if (projectModel != null) {
+                  return _buildProjectDetail(context, projectModel);
+                }
+                return const Center(
+                  child: Text('Erro ao carregar projetos'),
+                );
+            }
+          }),
+    );
+  }
+
+  Widget _buildProjectDetail(BuildContext context, ProjectModel projectModel) {
+    return CustomScrollView(
+      slivers: [
+        ProjectDetailAppbar(
+          projectModel: projectModel,
+        ),
+        SliverList(
+          delegate: SliverChildListDelegate(
+            [
+              const Padding(
+                padding: EdgeInsets.only(top: 50, bottom: 50),
+                child: ProjectPieChart(
+                  
                 ),
-                const ProjectTaskTile()
-              ],
-            ),
+              ),
+              const ProjectTaskTile()
+            ],
           ),
-          SliverFillRemaining(
-            hasScrollBody: false,
-            child: Align(
-              alignment: Alignment.bottomRight,
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: const Icon(JobTimerIcons.ok_circled2),
-                    label: const Text('Finalizar projeto')),
+        ),
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(JobTimerIcons.ok_circled2),
+                label: const Text('Finalizar projeto'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                    const Color(0xFF34A853),
+                  ),
+                ),
               ),
             ),
-          )
-        ],
-      ),
+          ),
+        )
+      ],
     );
   }
 }
