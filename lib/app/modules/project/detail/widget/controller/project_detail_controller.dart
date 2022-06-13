6a321c1eadff023dd9056a604689073677a886aa
Medeiros <hsymetry@gmail.com>
2022-06-13 12:19:32 -0300

    Implement project detail

---
+++ lib/app/modules/project/detail/widget/controller/project_detail_controller.dart
@@ -0,0 +1,23 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:job_timer/app/services/auth/projects/project_service.dart';
+import 'package:job_timer/app/view_models/project_model.dart';
+
+part 'project_detail_state.dart';
+
+class ProjectDetailController extends Cubit<ProjectDetailState> {
+  final ProjectService _projectService;
+  ProjectDetailController({required ProjectService projectService})
+      : _projectService = projectService,
+        super(ProjectDetailState.initial());
+
+  void setProject(ProjectModel projectModel) {
+    emit(state.copyWith(
+        projectModel: projectModel, status: ProjectDetailStatus.complete));
+  }
+
+  Future<void> updateProject() async {
+    final project = await _projectService.findById(state.projectModel!.id!);
+    emit(state.copyWith(projectModel: project));
+  }
+}
---
+++ lib/app/modules/project/detail/widget/controller/project_detail_state.dart
@@ -0,0 +1,28 @@
+part of 'project_detail_controller.dart';
+
+enum ProjectDetailStatus { initial, loading, complete, failure }
+
+class ProjectDetailState extends Equatable {
+  final ProjectDetailStatus status;
+  final ProjectModel? projectModel;
+
+  ProjectDetailState._({
+    required this.status,
+    this.projectModel,
+  });
+
+  ProjectDetailState.initial() : this._(status: ProjectDetailStatus.initial);
+
+  ProjectDetailState copyWith({
+    ProjectDetailStatus? status,
+    ProjectModel? projectModel,
+  }) {
+    return ProjectDetailState._(
+      status: status ?? this.status,
+      projectModel: projectModel ?? this.projectModel,
+    );
+  }
+
+  @override
+  List<Object?> get props => [status, projectModel];
+}
