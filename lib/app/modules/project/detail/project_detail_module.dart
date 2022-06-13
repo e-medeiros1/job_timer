27b22d9490373643f80972a963bd3f1a3325084b
Medeiros <hsymetry@gmail.com>
2022-06-13 12:19:55 -0300

    Implement module for project detail

--- lib/app/modules/project/detail/project_detail_module.dart
+++ lib/app/modules/project/detail/project_detail_module.dart
@@ -1,14 +1,21 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:job_timer/app/modules/project/detail/project_detail_page.dart';
+import 'package:job_timer/app/modules/project/detail/widget/controller/project_detail_controller.dart';
+import 'package:job_timer/app/view_models/project_model.dart';
+import 'package:modular_bloc_bind/modular_bloc_bind.dart';
 
 class ProjectDetailModule extends Module {
   @override
-  List<Bind> get binds => [];
+  List<Bind> get binds =>
+      [BlocBind.lazySingleton((i) => ProjectDetailController(projectService: i()))];
   @override
   List<ModularRoute> get routes => [
         ChildRoute(
           '/',
-          child: ((context, args) => ProjectDetailPage()),
+          child: ((context, args) {
+            final ProjectModel projectModel = args.data;
+            return ProjectDetailPage(controller: Modular.get()..setProject(projectModel));
+          }),
         )
       ];
 }
