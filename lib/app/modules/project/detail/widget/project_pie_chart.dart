3364a1ce031bac9861cce4d24162f1e8e9daa978
Medeiros <hsymetry@gmail.com>
2022-06-14 19:02:52 -0300

    Code review - PieChart refactoring

--- lib/app/modules/project/detail/widget/project_pie_chart.dart
+++ lib/app/modules/project/detail/widget/project_pie_chart.dart
@@ -12,40 +12,59 @@ class ProjectPieChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final double residual = (projectEstimate - totalTask);
-
     final theme = Theme.of(context);
+    var chartData = <PieChartSectionData>[];
+    //Se for maior que zero, ainda tem horas pra gastar
+    if (residual > 0) {
+      chartData = [
+        PieChartSectionData(
+          value: totalTask.toDouble(),
+          color: theme.primaryColor,
+          showTitle: true,
+          title: '${totalTask.toStringAsFixed(0)}h',
+          titleStyle: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        PieChartSectionData(
+          //Total de horas restantes
+          value: residual.toDouble(),
+          color: theme.primaryColorLight,
+          showTitle: true,
+          title: '${residual.toStringAsFixed(0)}h',
+          titleStyle: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ];
+    } else {
+      chartData = [
+        PieChartSectionData(
+          value: totalTask.toDouble(),
+          showTitle: true,
+          title: '${totalTask}h',
+          color: Colors.redAccent,
+          titleStyle: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        )
+      ];
+    }
+
     return SizedBox(
         height: 200,
         width: 200,
         child: Stack(
           fit: StackFit.loose,
           children: [
-            PieChart(PieChartData(
-              sections: [
-                //Total de horas já gastos
-                PieChartSectionData(
-                  value: totalTask.toDouble(),
-                  color: theme.primaryColor,
-                  showTitle: true,
-                  title: '${totalTask.toStringAsFixed(0)}h',
-                  titleStyle: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                PieChartSectionData(
-                  //Total de horas restantes
-                  value: residual.toDouble(),
-                  color: theme.primaryColorLight,
-                  showTitle: true,
-                  title: '${residual.toStringAsFixed(0)}h',
-                  titleStyle: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            )),
+            PieChart(
+              PieChartData(
+                sections: chartData,
+              ),
+            ),
             Align(
               //Total
               alignment: Alignment.center,
