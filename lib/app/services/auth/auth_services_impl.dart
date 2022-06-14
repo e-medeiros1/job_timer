44dad7246b9fff32d375c4634e4b390a6c23ae3d
Medeiros <hsymetry@gmail.com>
2022-06-14 17:46:08 -0300

    Cleaning database projects when make logout

--- lib/app/services/auth/auth_services_impl.dart
+++ lib/app/services/auth/auth_services_impl.dart
@@ -1,9 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
+import 'package:job_timer/app/core/database/database.dart';
+
 import './auth_services.dart';
 
 class AuthServicesImpl implements AuthServices {
+  final Database _database;
+  AuthServicesImpl({required Database database}) : _database = database;
+
   @override
   //Abrindo a tela para escolher email
   Future<void> signIn() async {
@@ -23,7 +28,13 @@ class AuthServicesImpl implements AuthServices {
   }
 
   @override
-  Future<void> signoUT() async {
+  Future<void> signOut() async {
+    //Pode ser feito utilizando o cascade notation
+    // await _database.openConnection()
+    //   ..clearSync();
+    final database = await _database.openConnection();
+    await database.writeTxn((isar) => database.clear());
+
     //Se chamar apenas esse método, ele não apresenta a janela para login
     //Já loga novamente pois o googleSignIn salva o último email logado
     await FirebaseAuth.instance.signOut();
